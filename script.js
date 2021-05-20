a84799f278093920349e672bae2f9a6c7355d580
Allen Lee <printallenlee@gmail.com>
2021-05-20 15:34:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // esse script é usado para implementar o dark mode 
 var on = window.document.querySelector('button#ligar')
 var off = window.document.querySelector('button#desligar')
- on.addEventListener('click',clicar)
+ on.addEventListener('click',clicar1)
  off.addEventListener('click',clicar2)
  
- function clicar() {
+ function clicar1() {
      
      bod.style.background = '#212529'
      te.style.background = '#899095'
